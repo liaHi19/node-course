8fffaa399c90dd3e4988c70f1ca55f1a5c198ba2
liaHi19 <gn.natalia19@gmail.com>
2022-12-14 14:18:03 +0200

    [#2] Add bodyParser, using redirect

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/", (req, res, next) => {
   console.log("This always runs");
   next();
 });
 
-app.use("/add-product", (req, res, next) => {
-  console.log("add product");
-  res.send("<h1>The Add-Product Page</h1>");
+app.use("/add-product", (req, res) => {
+  res.send(
+    "<form action='/product' method='POST'><input name='title' type='text'/> <button type='submit'>Submit</button></form>"
+  );
+});
+
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("Home page");
   res.send("<h1>Hello from Express</h1>");
 });
 
