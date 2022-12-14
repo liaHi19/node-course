4b087496c2c2beec08133eb7eae1946432e45a7a
liaHi19 <gn.natalia19@gmail.com>
2022-12-14 13:51:45 +0200

    [#1] Add app.use()

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+app.use("/", (req, res, next) => {
+  console.log("This always runs");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("add product");
+  res.send("<h1>The Add-Product Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Home page");
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(3000);
