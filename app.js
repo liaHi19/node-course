b1c339f3bf88c6241bd66b4a1c91cd081558149b
liaHi19 <gn.natalia19@gmail.com>
2022-12-14 15:42:54 +0200

    [#3] Add routes

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs");
-  next();
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/add-product", (req, res) => {
-  res.send(
-    "<form action='/product' method='POST'><input name='title' type='text'/> <button type='submit'>Submit</button></form>"
-  );
+app.use((req, res, next) => {
+  res.status(404).send("<h2>Page not found</h2>");
 });
-
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express</h1>");
-});
-
 app.listen(3000);
