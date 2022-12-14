ae2c1d28edd80d3a9842eb88467d6338399177b1
liaHi19 <gn.natalia19@gmail.com>
2022-12-14 16:32:38 +0200

    [#4] Add views folder with html-pages, add sending pages

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -11,6 +13,6 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h2>Page not found</h2>");
+  res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
 });
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
+const path = require("path");
+
 const express = require("express");
+
+const rootDir = require("../helpers/path");
+
 const router = express.Router();
 
 router.get("/add-product", (req, res) => {
-  res.send(
-    "<form action='/admin/add-product' method='POST'><input name='title' type='text'/> <button type='submit'>Submit</button></form>"
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res) => {
