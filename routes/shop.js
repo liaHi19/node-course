7dd651f2b39020380300965103d2610ddad062ec
Nataliia Hirniak <gn.natalia19@gmail.com>
2022-12-20 16:55:06 +0200

    [#6] Add converting html files to pug

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 const rootDir = require("../helpers/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const { products } = adminData;
+  res.render("shop", { products, docTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
