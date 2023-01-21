7d529dc87f56546a570c9e178fc3ce8d09dc29ef
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-01-21 23:17:58 +0200

    [#44] Add basic login

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,15 +18,26 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("63c6d933026aadfaa234ae8a")
+  const { email, password } = req.body;
+  User.findOne({ email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/");
+      if (!user) {
+        return res.redirect("/");
+      }
+      bcrypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+            res.redirect("/");
+          });
+        }
+        res.redirect("/login");
       });
     })
+    .catch((err) => console.log(err))
+    .then((user) => {})
     .catch((err) => console.log(err));
 };
 
